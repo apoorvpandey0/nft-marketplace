24981b6a2e26eab8cbd99a6864e1a431596acae4
apoorvpandey0 <apoorvpandey0@gmail.com>
2021-10-20 13:51:53 +0530

    initial setup

---
+++ hardhat.config.js
@@ -0,0 +1,27 @@
+/* hardhat.config.js */
+require("@nomiclabs/hardhat-waffle")
+const fs = require('fs')
+const privateKey = fs.readFileSync(".secret").toString().trim() || " "
+const projectId = process.env.PROJECT_ID
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    mumbai: {
+      url: "https://polygon-mumbai.infura.io/v3/${projectId}",
+      accounts: [privateKey]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+}
